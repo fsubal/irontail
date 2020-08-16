16b456a8cbf87e67308639bb483577fbb4589283
fsubal <fsubal1102@gmail.com>
2020-08-16 17:38:15 +0900

    cut down unused css parsing

--- src/TailwindClient.ts
+++ src/TailwindClient.ts
@@ -1,7 +1,7 @@
 import * as ts from "typescript/lib/tsserverlibrary";
 import * as fs from "fs";
 import * as path from "path";
-import extractClassNames from "./extractClassNames";
+import { extractClassNames } from "./extractClassNames";
 import importFrom = require("import-from");
 import resolveFrom = require("resolve-from");
 import type { Result } from "postcss";
@@ -35,6 +35,9 @@ export class TailwindClient {
     const postcss = this.requirePostCss();
     const tailwindcss = this.requireTailwindCss();
 
+    this.project.projectService.logger.info(
+      `start compile ${new Date().toISOString()}`
+    );
     return Promise.all(
       ["base", "components", "utilities"].map((group) =>
         postcss([tailwindcss(configPath)]).process(`@tailwind ${group};`, {
@@ -45,33 +48,50 @@ export class TailwindClient {
       TailwindClient.lastUpdatedAt = this.getLastUpdatedAt();
       TailwindClient.currentClasses = this.extractClassNames(result);
 
-      this.project.projectService.logger.info("compiled tailwind.css");
+      this.project.projectService.logger.info(
+        JSON.stringify(TailwindClient.currentClasses)
+      );
     });
   }
 
+  private extractClassNames([base, components, utilities]: Result[]) {
+    return extractClassNames([
+      { root: base.root, source: "base" },
+      { root: components.root, source: "components" },
+      { root: utilities.root, source: "utilities" },
+    ]);
+  }
+
+  private _postcss?: typeof import("postcss");
   private requirePostCss() {
+    if (this._postcss) {
+      return this._postcss;
+    }
+
     const configPath = this.getConfigPath();
     const tailwindRootPath = path.dirname(
       resolveFrom(configPath, "tailwindcss/package.json")
     );
 
-    return importFrom(tailwindRootPath, "postcss") as typeof import("postcss");
+    this._postcss = importFrom(
+      tailwindRootPath,
+      "postcss"
+    ) as typeof import("postcss");
+
+    return this._postcss;
   }
 
-  private extractClassNames([base, components, utilities]: Result[]) {
-    const { classNames } = extractClassNames([
-      { root: base.root, source: "base" },
-      { root: components.root, source: "components" },
-      { root: utilities.root, source: "utilities" },
-    ]);
-
-    return classNames;
-  }
-
+  private _tailwindcss?: Function;
   private requireTailwindCss() {
-    return importFrom(
+    if (this._tailwindcss) {
+      return this._tailwindcss;
+    }
+
+    this._tailwindcss = importFrom(
       path.dirname(this.getConfigPath()),
       "tailwindcss"
     ) as Function;
+
+    return this._tailwindcss;
   }
 }
